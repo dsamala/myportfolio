a2d486e40582a4dcfab005225f0f49b9e10a935c
Denzel <denzelsamala@Denzel-2.local>
2022-08-23 20:32:46 -0700

    added svg icons for project logos

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,3 +1,7 @@
+import rendevous from '../assets/svg/rendevous.svg';
+import soundgarden from '../assets/svg/soundgarden.svg';
+import theraspace from '../assets/svg/theraspace.svg';
+
 export const projectsData = [
     {
         id: 1,
@@ -5,6 +9,7 @@ export const projectsData = [
         projectDesc: 'A social media application that aims to connect junior developers with their respective peers and foster a positive environment that uses code as a platform to connect and collaborate.',
         code: 'https://github.com/PredatorR10/Rendevous',
         demo: 'https://rendevousss.herokuapp.com/',
+        projectImage: rendevous,
     },
     {
         id: 2,
@@ -12,6 +17,7 @@ export const projectsData = [
         projectDesc: 'An application that focuses on connecting user data using Spotify API with the purpose of creating playlists for upcoming concerts or festivals.',
         code: 'https://github.com/helenamarkus14/soundgarden',
         demo: 'http://thesoundgarden.herokuapp.com/',
+        projectImage: soundgarden,
     },
     {
         id: 3,
@@ -19,6 +25,7 @@ export const projectsData = [
         projectDesc: 'A simple patient management portal that provides basic administrative tools to create, read, update, and delete records of patients and/or clinicians. It aims to create a template electronic platform for small scale rehabilitation clinics that is user-friendly and scalable for future development',
         code: 'https://github.com/dsamala/theraspace',
         demo: 'https://theraspace.herokuapp.com',
+        projectImage: theraspace,
     }
 
 
